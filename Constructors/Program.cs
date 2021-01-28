8733bac6eaf7d038e19c9483a34a507d8d03c60e
Semih Arslan <semihars1616@gmail.com>
2021-01-28 20:48:29 +0300

    28 Ocak

---
+++ Constructors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Semih", LastName = "Arslan", City = "Bursa" };
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+            Customer customer2 = new Customer( 2, "Engin", "Demiroğ", "Ankara" );
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        // default constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> oyunlar = new MyDictionary<int, string>();
+            oyunlar.Add(130, "Counter-Strike: Global Offensive");
+            oyunlar.Add(160, "Grand Theft Auto V");
+            oyunlar.Add(250, "Cyberpunk 2077");
+        }
+    }
+}
