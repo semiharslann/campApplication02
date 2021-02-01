1160141bd5699cb153688b8efe8fcc071c3e8247
Semih Arslan <semihars1616@gmail.com>
2021-02-02 01:23:27 +0300

    2 Şubat

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager : BaseCustomerManager, ICustomerService
+    {
+        private ICustomerCheckService _customerCheckService;
+        public StarbucksCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+        public override void Save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid person");
+            }
+        } 
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+            employeeManager.Update();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(employeeManager);
+        }
+    }
+    interface IPersonManager
+    {
+        // Unimplemented operation
+        void Add();
+        void Update();
+    }
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            // Müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            // Personel ekleme kodları
+            Console.WriteLine("Personel eklendi");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi");
+        }
+    }
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
